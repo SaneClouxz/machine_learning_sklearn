46dbb9704d2cd06a2c850b9809d4c14511e1bbe1
SaneClouxz <miracleclouxz@gmail.com>
2021-03-18 04:36:49 -0700

    my first SVM model

---
+++ svm_model.py
@@ -0,0 +1,28 @@
+from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+iris = datasets.load_iris()
+
+# splitting data into features and targets
+X = iris.data
+y = iris.target
+
+# splitting data into training and testing, 20% data for training test size
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# building the model
+model = svm.SVC()
+
+# training the model
+model.fit(X_train, y_train)
+
+# getting the model predictions and getting accuracy result
+predictions = model.predict(X_test)
+acc = accuracy_score(y_test, predictions)
+
+# printing the predictions and all
+print(f'Prediction: {predictions}')
+print(f'Actual: {y_test}')
+print(acc)
